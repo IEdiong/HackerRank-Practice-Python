6185be9463999083f29dab4e5f670c4a3a3d37d9
iewebdevdes <iewebdevdes@gmail.com>
2020-12-18 00:20:25 +0100

    First commit

---
+++ Introduction/Arithmetic_Operators.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    sumNum = a + b
+    diff = a - b
+    prod = a * b
+
+    print(sumNum)
+    print(diff)
+    print(prod)
---
+++ Introduction/Python_Division.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    intDiv = a // b
+    floatDiv = a / b
+
+    print(intDiv)
+    print(floatDiv)
