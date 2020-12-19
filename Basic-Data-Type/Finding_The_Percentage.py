fc82d6c7226ce3e56dd1809ce1b3dcaa64d7050b
iewebdevdes <iewebdevdes@gmail.com>
2020-12-19 14:35:27 +0100

    still progressing

--- Basic-Data-Type/Finding_The_Percentage.py
+++ Basic-Data-Type/Finding_The_Percentage.py
@@ -3,3 +3,24 @@
 # a list of students. Print the average of the
 # marks array for the student name provided,
 # showing 2 places after the decimal.
+from decimal import Decimal
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+
+    if 2 <= n <= 10:
+        for _ in range(n):
+            name, *line = input().split()
+            line = line[:3]
+            student_marks[name] = [float(str) for str in line]
+            # scores = [float(str) for str in line]
+            # student_marks[name] = scores
+
+        query_name = input()
+        avg = sum(student_marks[query_name]) / len(student_marks[query_name])
+        avg = round(Decimal(avg), 2)
+        print(avg)
+
+
+    else: print("Make your first input a number between 2 to 10, inclusive.")
---
+++ Basic-Data-Type/List.py
@@ -0,0 +1,30 @@
+if __name__ == '__main__':
+    n = int(input())
+    lst = []
+    cmd = []
+    cmd_para = []
+
+    for _ in range(n):
+        c, *para = input().split()
+        cmd.append(c)
+        cmd_para.append([int(str) for str in para])
+
+    # print(cmd, cmd_para)
+
+    for k, v in zip(cmd, cmd_para):
+        if k == 'insert':
+            lst.insert(v[0],v[1])
+        elif k == 'print':
+            print(lst)
+        elif k == 'remove':
+            lst.remove(v[0])
+        elif k == 'append':
+            lst.append(v[0])
+        elif k == 'sort':
+            lst.sort()
+        elif k == 'pop':
+            lst.pop()
+        elif k == 'reverse':
+            lst.reverse()
+
+    # print(lst)
