b912fcf4262c6974f929cb9dd66687073d6abf7b
iewebdevdes <iewebdevdes@gmail.com>
2020-12-19 01:43:32 +0100

    In progress

---
+++ Basic-Data-Type/Finding_The_Percentage.py
@@ -0,0 +1,5 @@
+# The provided code stub will read in a dictionary
+# containing key/value pairs of name:[marks] for
+# a list of students. Print the average of the
+# marks array for the student name provided,
+# showing 2 places after the decimal.
